04ed38bdca7423ec4c5411b5bc3e70d80897572e
Candice Canoso <candice.canoso@dtn.com>
2023-02-15 16:25:09 +0800

    Refactor masks

--- src/pages/cupcakes.tsx
+++ src/pages/cupcakes.tsx
@@ -3,7 +3,7 @@ import { Container, Typography } from "@mui/material";
 
 export default function CupcakesPage() {
   return (
-    <Container maxWidth="md" sx={{ mt: 6 }}>
+    <Container maxWidth="md" sx={{ pt: 10, pb: 10 }}>
       <Typography sx={{ mb: 3 }} variant="h2">
         Cupcakes page!
       </Typography>
