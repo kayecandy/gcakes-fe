965d8626e8f12348f8bad76277b1cc7f6c37724b
Candice Canoso <candice.canoso@dtn.com>
2023-02-15 16:06:16 +0800

    Fix build error

--- src/components/common/urls.ts
+++ src/components/common/urls.ts
@@ -4,7 +4,7 @@ const { publicRuntimeConfig } = getConfig();
 
 export const BACKEND_URL = publicRuntimeConfig.BACKEND_URL;
 
-export const GET_FEATURED_PRODUCTS_URL = (productType) =>
+export const GET_FEATURED_PRODUCTS_URL = (productType: string) =>
   `${BACKEND_URL}/products/featured/cakes`;
 
 export const GET_CAKES_URL = `${BACKEND_URL}/api/products/cakes`;
