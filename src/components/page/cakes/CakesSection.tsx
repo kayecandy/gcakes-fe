90075366d373b6b645a3ef5624e8253aa6c67955
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2023-02-17 17:51:56 +0800

    Trying to copy the wireframe

--- src/components/page/cakes/CakesSection.tsx
+++ src/components/page/cakes/CakesSection.tsx
@@ -3,18 +3,7 @@ import { FC } from 'react';
 import { Typography } from '@mui/material';
 import { Container } from '@mui/system';
 
-
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import CakesComponent from "./CakesComponent";
-
 import { useCakes } from './hooks/useCakes';
-
-import style from './CakesSection.module.css';
-import { grey } from '@mui/material/colors';
-
 
 /**
  * Use FeaturedSection as basis
@@ -22,7 +11,7 @@ import { grey } from '@mui/material/colors';
 export const CakesSection: FC = () => {
   const cakes = useCakes();
 
-  console.log(cakes)
+  console.log(cakes);
 
   return (
     <Container maxWidth="xl">
@@ -30,7 +19,7 @@ export const CakesSection: FC = () => {
 
       {/* Add cakes component here */}
 
-      {cakes.loading ? (
+      {/* {cakes.loading ? (
         <>Add cakes loading view here</>
 
 
@@ -45,82 +34,22 @@ export const CakesSection: FC = () => {
         </Grid>
       ) : (
         <>Add cakes error component here</>
-      )}
+      )} */}
 
-      {/* <Grid container spacing={2} columns={{ md: 12 }} sx={{
-        bgcolor: '#f9e4e4',
-      }}>
-        {testCakeData.map((item) => (
-          <CakesComponent cakeName={item.cakeName} imageName={item.imageName} />
-        ))}
-      </Grid> */}
-
-
-      {/* <Grid container spacing={2} columns={{ md: 12 }}>
-        <Grid item md={4}>
-          <Item>md=4</Item>
-        </Grid>
-        <Grid item md={4}>
-          <Item>md=4</Item>
-        </Grid>
-        <Grid item md={4}>
-          <Item>md=4</Item>
-        </Grid>
-        <Grid item md={12}>
-          <Item>md=12</Item>
-        </Grid>
-      </Grid> */}
-
-      {/* <div className={style.container}>
-        <div className={style.item}>
-          <img className={style.itemImg} src="Cake3.png" alt="item 1" />
-          <button className={style.btn}>
-            Item 1
-            <img className={style.btnImg} src="Cake3.png" alt="Item 1 btnImg" />
-          </button>
+      <div className="container">
+        <div className="item">
+          <img className="itemImg" src="Cake3.png" alt="item 1" />
+          <button>Button 1</button>
         </div>
-        <div className={style.item}>
-          <img className={style.itemImg} src="Cake3.png" alt="item 2" />
-          <button className={style.btn}>
-            Item 2
-            <img className={style.btnImg} src="Cake3.png" alt="Item 1 btnImg" />
-          </button>
+        <div className="item">
+          <img className="itemImg" src="Cake3.png" alt="item 2" />
+          <button>Button 2</button>
         </div>
-        <div className={style.item}>
-          <img className={style.itemImg} src="Cake3.png" alt="item 3" />
-          <button className={style.btn}>
-            Item 3
-            <img className={style.btnImg} src="Cake3.png" alt="Item 1 btnImg" />
-          </button>
+        <div className="item">
+          <img className="itemImg" src="Cake3.png" alt="item 3" />
+          <button>Button 3</button>
         </div>
       </div>
-
-      <div className={style.container}>
-        <div className={style.item}>
-          <img className={style.itemImg} src="Cake3.png" alt="item 1" />
-          <button className={style.btn}>
-            Item 1
-            <img className={style.btnImg} src="Cake3.png" alt="Item 1 btnImg" />
-          </button>
-        </div>
-        <div className={style.item}>
-          <img className={style.itemImg} src="Cake3.png" alt="item 2" />
-          <button className={style.btn}>
-            Item 2
-            <img className={style.btnImg} src="Cake3.png" alt="Item 1 btnImg" />
-          </button>
-        </div>
-        <div className={style.item}>
-          <img className={style.itemImg} src="Cake3.png" alt="item 3" />
-          <button className={style.btn}>
-            Item 3
-            <img className={style.btnImg} src="Cake3.png" alt="Item 1 btnImg" />
-          </button>
-        </div>
-      </div> */}
-
-
-
     </Container>
   );
 };
