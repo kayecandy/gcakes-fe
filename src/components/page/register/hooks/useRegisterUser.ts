80fba71496ffa8f057f4c7c5aaf48a9f9c8263a3
Candice Canoso <candice.canoso@dtn.com>
2023-02-25 16:27:27 +0800

    Fix build error

--- src/components/page/register/hooks/useRegisterUser.ts
+++ src/components/page/register/hooks/useRegisterUser.ts
@@ -1,30 +1,24 @@
-import {
-    useState,
-    useEffect,
-    SetStateAction,
-    Dispatch,
-} from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import { ApiResponse } from "@/types/api-response";
 import { User } from "@/types/user";
 
+export const useRegisterUser = (): [
+  ApiResponse<User[]>,
+  Dispatch<SetStateAction<ApiResponse<User[]>>>
+] => {
+  const [registerUser, setRegisterUser] = useState<ApiResponse<User[]>>({
+    loading: true,
+  });
 
-export const useRegisterUser = (): [User, Dispatch<SetStateAction<User>>] => {
-    const [registerUser, setRegisterUser] = useState<
-        ApiResponse<User[]>
-    >({
-        loading: true,
-    });
+  useEffect(() => {
+    const t = fetch("/api/users/register/user");
+  }, []);
 
-    useEffect(() => {
-        const t = fetch("/api/users/register/user")
-
-    }, []);
-
-    /*
+  /*
     useEffect(() => {
 
     }, [registerUser])*/
 
-    return [registerUser, setRegisterUser];
-}
+  return [registerUser, setRegisterUser];
+};
