8ad038a65a95ffff8111ea4ed82c847a7a0bacde
Jasper-Chua <75113066+Jasper-Chua@users.noreply.github.com>
2023-03-02 20:49:25 +0800

    1

--- src/components/page/cakes/CakesSection.tsx
+++ src/components/page/cakes/CakesSection.tsx
@@ -25,13 +25,7 @@ import { grey } from '@mui/material/colors';
 export const CakesSection: FC = () => {
   const cakes = useCakes();
 
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+
 
   const testCakeData = [
     {
