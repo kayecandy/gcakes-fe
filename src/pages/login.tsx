6eb789eb626c7fc76515cd5b3f3cd1847d2ca46a
akn5 <104754302+alankun5@users.noreply.github.com>
2023-03-04 16:34:31 +0800

    Add: Login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/page/login/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <>
+            <LoginForm></LoginForm>
+        </>
+    )
+}
