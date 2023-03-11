5ecda1a55e3c80425563050edcf690635452b40a
Candice Canoso <candice.canoso@dtn.com>
2023-03-11 08:12:05 +0800

    Update cakes page unit test

--- tests/unit/pages/cakes.test.tsx
+++ tests/unit/pages/cakes.test.tsx
@@ -1,23 +1,35 @@
+import { ProductGrid } from '@/components/common/product/ProductGrid';
 import CakePage from '@/pages/cakes';
 import {
   render,
   screen,
 } from '@testing-library/react';
 
-jest.mock("@/components/page/cakes/CakesSection", () => {
+jest.mock('@/components/common/product/ProductGrid', () => {
+  const ProductGridMock: typeof ProductGrid = ({productType}) => {
+    return <div data-testid="cakes-product-grid" data-product-type={productType}></div>
+  }
+  
   return {
-    CakesSection() {
-      return <div data-testid="cakes-section"></div>;
-    },
-  };
+    ProductGrid: ProductGridMock
+  }
 });
 
+
 describe("Cakes Page", () => {
+  let cakesProductGrid: HTMLElement;
   beforeAll(() => {
     render(<CakePage />);
+
+    cakesProductGrid = screen.getByTestId("cakes-product-grid");
+
   });
 
-  test("Renders Sections", () => {
-    expect(screen.getByTestId("cakes-section")).toBeInTheDocument();
+  test("Renders ProductGrid component", () => {
+    expect(cakesProductGrid).toBeInTheDocument();
   });
+
+  test("ProductGrid component has correct productType", () => {
+    expect(cakesProductGrid.getAttribute('data-product-type')).toBe("cakes")
+  })
 });
