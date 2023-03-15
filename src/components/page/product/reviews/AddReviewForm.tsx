a40c1536d7630669daa9e7ae8f3fda202568b6c7
Candice Canoso <candice.canoso@dtn.com>
2023-03-16 03:06:08 +0800

    AddReviewForm initial

--- src/components/page/product/reviews/AddReviewForm.tsx
+++ src/components/page/product/reviews/AddReviewForm.tsx
@@ -1,10 +1,45 @@
-import { Box } from "@mui/material";
+import { Box, Button, Grid, Input, Rating, TextField, Typography } from "@mui/material";
 import { FC } from "react";
 
 export const AddReviewForm: FC = () => {
-  return <Box sx={{
-    mt: 20
-  }}>
-    This is the add review form
-  </Box>
-}
+  return (
+    <Box
+      sx={{
+        mt: 20,
+        background: "white",
+        p: 3,
+        borderRadius: 4,
+      }}
+      maxWidth="sm"
+    >
+      <Typography
+        sx={{
+          mb: 3,
+        }}
+        variant="h4"
+      >
+        Let us know what you think!
+      </Typography>
+
+      <Grid container columnGap={3}>
+        <Grid
+          sx={{
+            flexGrow: 1,
+            textAlign: "center"
+          }}
+          item
+        >
+          <Box sx={{ textAlign: "center",  mb:3}}>
+            <Rating size="large"></Rating>
+          </Box>
+
+          <TextField sx={{ mb: 3 }} label="Title" fullWidth></TextField>
+          <TextField label="Comment" fullWidth multiline rows={4}></TextField>
+
+          <Button sx={{mt:2}} size="large" variant="contained">Submit</Button>
+        </Grid>
+        <Grid xs="auto" item></Grid>
+      </Grid>
+    </Box>
+  );
+};
