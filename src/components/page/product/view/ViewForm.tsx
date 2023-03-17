cb533b5f66171ec0153541787a4c1f394975584a
akn5 <104754302+alankun5@users.noreply.github.com>
2023-03-17 16:42:07 +0800

    [STSWENG-125] Updates: View product page initial, changed directory

--- src/components/page/product/view/ViewForm.tsx
+++ src/components/page/product/view/ViewForm.tsx
@@ -1,251 +1,128 @@
 import { SX_MASKS } from "@/components/common/masks";
-import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Box, Button, Card, CardActionArea,
-    CardActions, CardContent, CardMedia,
-    Chip, CircularProgress, Container, Grid,
-    IconButton, Modal, Stack, Typography
-} from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { useEffect, useState } from "react";
+import { Box, Button, Container, Grid, Rating, TextField, Typography } from "@mui/material";
+import { FC } from "react";
 import { COLOR_PALLETE } from "../../ThemeProvider";
-import { useViewedProduct } from "./hooks/useViewedProduct";
-import ProductReview from "./ProductReview";
-import { Product } from "@/types/product";
-import { ApiResponse } from "@/types/api-response";
-import { Review } from "@/types/review";
-import { useViewedReviews } from "./hooks/useViewedReviews";
-
-const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    borderRadius: '2rem',
-    boxShadow: 20,
-    width: 1000,
-    height: 550,
-}
 
 type ViewProps = {
-    productId: string | any,
+    productType: string | any,
 };
 
-const ViewForm = ({ productId }: ViewProps) => {
-    const viewedProduct = useViewedProduct(productId);
-    const viewedReviews = useViewedReviews(productId);
-    const [activeProduct, setActiveProduct] = useState<ApiResponse<Product>>({ loading: true });
-    const [open, setOpen] = useState(false);
-    const [activeReviews, setActiveReviews] = useState<ApiResponse<Review[]>>({ loading: true });
-    
-    useEffect(() => {
-        //console.log("Viewing product", viewedProduct);
-        setActiveProduct({
-            loading: false,
-            value: viewedProduct.value,
-        });
-    }, [viewedProduct]);
+const ViewForm = ({ productType }: ViewProps) => {
 
-    useEffect(() => {
-        console.log("Viewed reviews", viewedReviews);
-        setActiveReviews({
-            loading: false, 
-            value: viewedReviews.value,
-        })
-    }, [viewedReviews]);
+    console.log("props: ", productType);
 
-    useEffect(() => {
-        console.log("Active product", activeProduct);
-        
-    }, [activeProduct]); 
-
-    useEffect(() => {
-        console.log("Active reviews", activeReviews);
-    }, [activeReviews]);
-
-    // When user clicks a tag
-    const handleTagClick = () => {
-        console.info('Tag clicked.');
-    }
-
-    // When user clicks the product photo
-    const handleOpen = () => {
-        console.info('Modal opened.');
-        setOpen(true);
-    }
-
-    const handleClose = () => {
-        console.info('Modal closed.');
-        setOpen(false);
-    }
-    
     return (
-        <Box // outer background
-            sx={{
-                ...SX_MASKS[1]("bottom"),
-                position: "relative",
-                overflow: "hidden",
-                backgroundColor: `${COLOR_PALLETE[0]}C0`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-                backgroundPositionX: "right",
-                backgroundPositionY: "90%",
-                //display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "5%",
-                mb: "5%",
-                //height: "120vh",
-                zIndex: 0,
+        <Box
+        sx={{
+            ...SX_MASKS[1]("bottom"),
+            position: "relative",
+            overflow: "hidden",
+            backgroundColor: `${COLOR_PALLETE[0]}C0`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPositionX: "right",
+            backgroundPositionY: "90%",
+            //display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "5%",
+            mb: "5%",
+            //height: "120vh",
+            zIndex: 0,
             }}
         >
-            { viewedProduct.loading ? (
-                <div style={{ position: 'relative', top: '50%', left: '50%'}}>
-                    <CircularProgress />
-                    <Typography>
-                        Loading...
-                    </Typography>
-                </div>
-            ):
-                viewedProduct.value ? (
-                    <div>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}
-                        >
-                            <Box sx={modalStyle} zIndex={1000}>
-                                <IconButton onClick={handleClose} sx={{display: 'block', position: 'relative', left: '95%', top: '1%'}}>
-                                    <CloseOutlinedIcon />
-                                </IconButton>
-                                
-                                <div style={{position: 'relative', top: '25%', left: '50%', transform: 'translate(-5%, 0%)'}}>
-                                    <Typography variant="h5">Modal Text</Typography>
-                                </div>
-                                 
-                            </Box>
-                        </Modal>
+            <Typography variant="h6">
+                Item: {productType}
+            </Typography>
 
-                        <div style={{ display: "inline-flex", }}>
-                            <Typography variant="h6" sx={{ mr: "15px", }}>
-                                Tags:
-                            </Typography>
-                            <Stack direction="row" spacing={1}>
-                                <Chip label={String(viewedProduct.value.productType).toUpperCase()} onClick={handleTagClick} color="primary" />
-                                <Chip label="Tag 2" onClick={handleTagClick} />
-                                <Chip label="Tag 3" onClick={handleTagClick} />
-                            </Stack>
-                        </div>
-                        
-                        <div    // Contents | White Container
-                            style={{
-                                flexGrow: 1, 
-                                backgroundColor: `white`,
-                                padding: "3vw",
-                                borderRadius: "2rem",
-                                borderWidth: "5px",
-                                borderColor: COLOR_PALLETE[0],
-                                borderStyle: "solid",
+            <div    // Contents
+                style={{
+                    flexGrow: 1,
+                    backgroundColor: `white`,
+                    padding: "3vw",
+                    borderRadius: "2rem",
+                    borderWidth: "5px",
+                    borderColor: COLOR_PALLETE[0],
+                    borderStyle: "solid",
+                }}
+            >
+                <Container
+                    sx={{
+                        textAlign: "center",
+                        pb: 10,
+                    }}
+                        maxWidth="sm"
+                >
+                    <Grid container
+                        sx={{
+                            mb: "5%"
+                        }}
+                    >
+                        <Grid item xs={8}
+                            sx={{
+                                backgroundColor: `gray`,
                             }}
                         >
-                            <Container
-                                sx={{
-                                    display: "flex",
-                                    //backgroundColor: `gray`, //
-                                    maxHeight: 750,
-                                }}
-                                maxWidth="xl"
+                            Thumbnail
+                        </Grid>
+                        <Grid item xs={4}
+                            sx={{
+                                backgroundColor: `pink`,
+                            }}
+                        >
+                            Details Pane
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                color="success"
+                                fullWidth
                             >
-                                <Grid container spacing={0}
-                                    sx={{
-                                        maxWidth: "100%",
-                                        //backgroundColor: 'orange', //
-                                        overflowY: "auto",
-                                        //maxHeight: 650,
-                                        display: "flex",
-                                        justifyContent: "center",
-                                    }}
-                                >
-                                    {/** Product Details Section **/}
-                                    <Grid item>
-                                        <Card sx={{position: "sticky", top: 0, }}>
-                                            <CardActionArea>
-                                                <CardMedia sx={{ maxHeight: 450, maxWidth: 550 }}
-                                                    component="img"
-                                                    image={viewedProduct.value.image?.url}
-                                                    alt="product"
-                                                    onClick={handleOpen}
-                                                />
-                                                <CardContent sx={{ maxWidth: 450 }}>
-                                                <Typography gutterBottom variant="h5" component="div">
-                                                    {viewedProduct.value.name}
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary">
-                                                    {viewedProduct.value.description}
-                                                </Typography>
-                                                </CardContent>
-                                            </CardActionArea>
-                                            <CardActions>
-                                                <Button
-                                                    //variant="contained"
-                                                    size="large"
-                                                    color="success"
-                                                    fullWidth
-                                                    onClick={handleOpen}
-                                                > 
-                                                    <ShoppingCartIcon fontSize="medium" /> &nbsp; Add to Cart
-                                                </Button>
-                                            </CardActions>
-                                        </Card>
-                                    </Grid>
-
-                                    {/** Review Section **/}
-                                    <Grid item xs={6}> 
-                                        <Container>
-                                            <Typography variant="h4" sx={{ mt: 2, }}>
-                                                Reviews
-                                            </Typography>
-                                            {/** Review Components **/}
-                                            <div style={{
-                                                maxWidth: "100%",
-                                            }}>
-                                                
-                                                <Stack>
-                                                    {viewedReviews.loading ? (
-                                                        <div style={{ position: 'relative', }}>
-                                                            <CircularProgress />
-                                                            <Typography>
-                                                                Loading...
-                                                            </Typography>
-                                                        </div>
-                                                    ) : 
-                                                        viewedReviews.value ? (
-                                                            <>
-                                                                {viewedReviews.value.map((review) => (
-                                                                        <ProductReview key={review.sys.id} review={review} />
-                                                                ))}
-                                                            </> 
-                                                        ) : (
-                                                            <>Errored...</>   
-                                                    )}
-                                                </Stack>
-                                            </div>
-                                        </Container>
-                                    </Grid>
-                                </Grid>
-                            </Container>
-                        </div>  
-                    </div>
-            ) : ( // errored
-                <div>
-                    Error...        
-                </div>
-            )}
+                                Add to Cart
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    
+                    <Typography variant="h6">
+                        Reviews for { productType }
+                    </Typography>
+                    <Grid container
+                        sx={{
+                            //mb: "5%"
+                        }}
+                    >
+                        <Grid item xs={9}
+                            sx={{
+                                backgroundColor: `${COLOR_PALLETE[0]}C0`,
+                            }}
+                        >
+                            <TextField
+                                label="readOnly"
+                                defaultValue="Comments textbox"
+                                multiline
+                                rows={4}
+                                fullWidth
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={3}
+                            sx={{
+                                //backgroundColor: `white`,
+                            }}
+                        >
+                            <Rating
+                                name="simple-controlled"
+                                value={3}
+                                readOnly
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
+            
         </Box>
-    );
+    )
 }
 
 export default ViewForm;
