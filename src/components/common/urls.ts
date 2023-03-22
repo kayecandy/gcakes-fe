c3702a05375943094ebb2bb6f8341d5db9b8d559
akn5 <104754302+alankun5@users.noreply.github.com>
2023-03-22 17:36:39 +0800

    [STSWENG-125] Update: Review functionality

--- src/components/common/urls.ts
+++ src/components/common/urls.ts
@@ -12,8 +12,10 @@ export const GET_PRODUCT_URL = (productType: ProductTypes) => `${BACKEND_URL}/pr
 
 export const GET_CAKES_URL = `${BACKEND_URL}/products/cakes`;
 
-// test
 export const GET_VIEW_PRODUCT_URL = (productId: string) => `${BACKEND_URL}/products/view/${productId}`;
+export const GET_ALLREVIEWS_URL = `${BACKEND_URL}/products/getReviews`;
+// backend not yet implemented
+export const GET_REVIEWS_URL = (productId: string) => `${BACKEND_URL}/reviews/${productId}`;
 
 export const REGISTER_URL = `${BACKEND_URL}/users/register/user`;
 export const LOGIN_URL = `${BACKEND_URL}/users/login`;
