97dda566aedb64ce593097215f36eb739f2c9961
Candice Canoso <candice.canoso@dtn.com>
2023-03-22 19:42:21 +0800

    Update product type URLs

--- src/components/common/urls.ts
+++ src/components/common/urls.ts
@@ -8,13 +8,13 @@ export const BACKEND_URL = publicRuntimeConfig.BACKEND_URL;
 export const GET_FEATURED_PRODUCTS_URL = (productType: ProductTypes) =>
   `${BACKEND_URL}/products/featured/${productType}`;
 
-export const GET_PRODUCT_URL = (productType: ProductTypes) => `${BACKEND_URL}/products/${productType}`;
+export const GET_PRODUCT_URL = (productType: ProductTypes) => `${BACKEND_URL}/products/type/${productType}`;
 
-export const GET_CAKES_URL = `${BACKEND_URL}/products/cakes`;
+export const GET_CAKES_URL = `${BACKEND_URL}/products/type/cakes`;
 
-export const GET_CUPCAKES_URL = `${BACKEND_URL}/products/cupcakes`;
+export const GET_CUPCAKES_URL = `${BACKEND_URL}/products/type/cupcakes`;
 
-export const GET_DECORATED_COOKES_URL = `${BACKEND_URL}/products/decorated_cookies`;
+export const GET_DECORATED_COOKES_URL = `${BACKEND_URL}/products/type/decorated_cookies`;
 
 
 export const REGISTER_URL = `${BACKEND_URL}/users/register/user`;
