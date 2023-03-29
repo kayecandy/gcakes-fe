c0ca7997d569deff5e3f8eb0b629b6b90073abd8
akn5 <104754302+alankun5@users.noreply.github.com>
2023-03-29 15:07:14 +0800

    [STSWENG-125] View Product Page (Re-PR) (#27)
    * Add: Product view page initial commit
    
    * [STSWENG-125] Updates: View product page initial, changed directory
    
    * init
    
    * update: viewform corrections
    
    * [STSWENG-125]: Added View Product Page
    
    * Update ViewForm.tsx
    
    * [STSWENG-125] Fix: Deployment
    
    * [STSWENG-125] Fixes: Component positioning; Add: Clickable content
    
    * [STSWENG-125] Update: Review functionality
    
    * Add: Product view page initial commit
    
    * [STSWENG-125] Updates: View product page initial, changed directory
    
    * init
    
    * update: viewform corrections
    
    * [STSWENG-125]: Added View Product Page
    
    * Update ViewForm.tsx
    
    * [STSWENG-125] Fix: Deployment
    
    * [STSWENG-125] Fixes: Component positioning; Add: Clickable content
    
    * [STSWENG-125] Update: Review functionality
    
    * * Added link to ProductComponent
    * Refactor ViewFormProps.productId type to string
    * Added router.isReady check in ViewPage
    
    * Update urls.ts
    
    * [STSWENG-125] Fix: Merge conflicts, Image fitting
    
    * Merge Conflicts Resolved
    
    * [STSWENG-125] Fix: url
    
    * [STSWENG-125] Fix: Vercel deployment
    
    * [STSWENG-125] Fix: Vercel Deployment (Last na to pls)
    
    ---------
    
    Co-authored-by: Candice Canoso <candice.canoso@dtn.com>

---
+++ src/components/page/product/view/hooks/useViewedReviews.ts
@@ -0,0 +1,46 @@
+import { GET_REVIEWS_URL } from "@/components/common/util/urls";
+import { ApiResponse } from "@/types/api-response";
+import { Review } from "@/types/review";
+import { useEffect, useState } from "react";
+
+export const useViewedReviews = (productId: string) => {
+    /** TODO */
+    /** Returns a list of Reviews given a productId associated with the review */
+    const [viewedReviews, setViewedReviews] = useState<
+    ApiResponse<Review[]>
+  >({
+    loading: true,
+  });
+
+    useEffect(() => {
+        setViewedReviews({
+            loading: true,
+        });
+
+        fetch(GET_REVIEWS_URL(productId))
+            .then(async (res) => {
+                if (!res.ok) {
+                    throw await res.json();
+                }
+                
+                return res.json().then((result) => {
+                    console.log(result);
+
+                    setViewedReviews({
+                        loading: false,
+                        value: result,
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("errored", error);
+
+                setViewedReviews({
+                    loading: false,
+                    error,
+                });
+            });
+    }, []);
+
+    return viewedReviews;
+}
